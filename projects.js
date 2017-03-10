3c67305bb6d1ce1914f6137a83924d684ad31940
Richard Perdaan <richardperdaan@iMac-van-Beheerder-273.local>
2017-03-10 12:27:05 +0100

    Extra author
    Reindert added as author

--- projects.js
+++ projects.js
@@ -10,6 +10,7 @@
  * https://github.com/myparcelnl
  *
  * @author      Richard Perdaan <richard@myparcel.nl>
+ * @author 		Reindert Vetter <reindert@myparcel.nl>
  * @copyright   2017 MyParcel
  * @link        https://github.com/myparcelnl/github-projects
  */
